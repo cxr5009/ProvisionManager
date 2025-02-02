59666006e623d2b7014da5d168baf3ec85c464f9
Cody Reed <cxr5009@gmail.com>
2025-02-02 21:47:02 +0000

    Implement ADB installation and update processes for multiple operating systems

--- plugins/requirements/adb.go
+++ plugins/requirements/adb.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os/exec"
+	"runtime"
 )
 
 // ADBPlugin implements core.RequirementPlugin for ADB.
@@ -24,13 +25,51 @@ func (p *ADBPlugin) Check() error {
 
 func (p *ADBPlugin) Install() error {
 	fmt.Println("Installing ADB...")
-	// TODO: installation process
+
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("powershell", "choco install adb")
+	case "darwin":
+		cmd = exec.Command("brew", "install", "android-platform-tools")
+	case "linux":
+		cmd = exec.Command("sudo", "apt-get", "install", "-y", "adb")
+	default:
+		return errors.New("unsupported operating system")
+	}
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to install ADB: %s\n%s", err, output)
+	}
+
+	fmt.Println("ADB installed successfully.")
 	return nil
 }
 
 // Update ADB if needed.
 func (p *ADBPlugin) Update() error {
 	fmt.Println("Updating ADB...")
-	// TODO: update process
+
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("powershell", "choco upgrade adb")
+	case "darwin":
+		cmd = exec.Command("brew", "upgrade", "android-platform-tools")
+	case "linux":
+		cmd = exec.Command("sudo", "apt-get", "update", "&&", "sudo", "apt-get", "upgrade", "-y", "adb")
+	default:
+		return errors.New("unsupported operating system")
+	}
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to update ADB: %s", string(output))
+	}
+
+	fmt.Println("ADB updated successfully")
 	return nil
 }
